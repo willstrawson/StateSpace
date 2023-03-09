eb774e13fa02ec3631187c5513a98e415f798b96
willstrawson <ws231@sussex.ac.uk>
2023-03-09 14:15:22 -0500

    added functionality to a) supply own task maps b) reshape nifti object to match mask

--- StateSpace/CorrelateTasksWithGradients.py
+++ StateSpace/CorrelateTasksWithGradients.py
@@ -39,11 +39,18 @@ def getdata():
 
 
 # this function correlates task maps and gradient maps
-def corrTasks(outputdir, corr_method='spearman', saveMaskedimgs = False):
+def corrTasks(outputdir, inputfiles=None, corr_method='spearman', saveMaskedimgs = False):
 
     # get all the relevent data by calling getdata() function
-    gradient_paths, gradient_mask_path, task_paths = getdata() # Get all the data paths you need
-    print(gradient_mask_path)    
+    if inputfiles is None:
+        gradient_paths, gradient_mask_path, task_paths = getdata() # Get all the data paths you need
+    elif inputfiles:
+        assert os.path.exists(os.path.dirname(inputfiles[0]))
+        print(f"Using {len(inputfiles)} input task maps")
+        gradient_paths, gradient_mask_path, task_paths = getdata()
+        task_paths = inputfiles
+
+
     # load mask as nib object once 
     maskimg = nib.load(gradient_mask_path)
 
@@ -61,8 +68,15 @@ def corrTasks(outputdir, corr_method='spearman', saveMaskedimgs = False):
         task_name = task_name.split(".")[0]
 
         # apply mask 
-        multmap = nimg.math_img('a*b',a=taskimg, b=maskimg) #element wise multiplication 
-        
+        try:
+            multmap = nimg.math_img('a*b',a=taskimg, b=maskimg) #element wise multiplication 
+        except ValueError: # if shapes don't match
+            print('Shapes of images do not match')
+            print(f'mask image shape: {maskimg.shape}, task image shape {taskimg.shape}')
+            print('Reshaping task to mask image dimensions...')
+            taskimg = nimg.resample_to_img(source_img=taskimg,target_img=maskimg,interpolation='nearest')
+            multmap = nimg.math_img('a*b',a=taskimg, b=maskimg) #element wise multiplication 
+
         # if you want to save masked task images, set to true
         if saveMaskedimgs == True:
             nib.save(multmap, 
@@ -70,10 +84,10 @@ def corrTasks(outputdir, corr_method='spearman', saveMaskedimgs = False):
 
         # turn to numpy array 
         task_array_masked = multmap.get_fdata()
-                
+
         # create 1st level dictionary key (task name)
         corr_dictionary[task_name] = {}
-        
+
         print (task_name)
         print('\n')
 
@@ -82,27 +96,27 @@ def corrTasks(outputdir, corr_method='spearman', saveMaskedimgs = False):
 
             grad_name = os.path.basename(os.path.normpath(gradient))
             grad_name = grad_name.split(".")[0]
-            
+
             print (grad_name)
-            
+
             # load gradient
-            gradientimg = nib.load(gradient)
-                 
+            gradientimg = nib.load(gradient)                
+
             # Get the gradient image data as a numpy array
             gradient_array = gradientimg.get_fdata()
-            
+
             # correlate task map and gradients 
             if corr_method == 'spearman':
                 corr = spearmanr(gradient_array.flatten(), task_array_masked.flatten())[0]
 
             elif corr_method == 'pearson':
                 corr = pearsonr(gradient_array.flatten(), task_array_masked.flatten())[0]
-                
+
             print ("Raw correlation:",corr)
-                
+
             # apply fishers-r-to-z transformation to correlation value
             corr = np.arctanh(corr)
-            
+
             print ("Fisher r-to-z transformed correlation:",corr)
 
             # plot correlation of flattened arrays [mostly for testing but keeping for now]
@@ -111,13 +125,13 @@ def corrTasks(outputdir, corr_method='spearman', saveMaskedimgs = False):
             #plt.close()
 
             corr_dictionary[task_name][grad_name]= corr # add corr value to dict
-    
+
     # store results in transposed dataframe
     df = pd.DataFrame(corr_dictionary).T
     df.index.name = 'Task_name'
     # save dataframe to csv
-    df.to_csv(os.path.join(outputdir,f'gradscores_{corr_method}.tsv'),sep='\t')
-    
+    df.to_csv(os.path.join(outputdir,f'gradscores_{corr_method}.csv'))
+
     return df
 
 
--- src/call_CorrelateTasksWithGradients.py
+++ src/call_CorrelateTasksWithGradients.py
@@ -4,12 +4,15 @@
 Create binarized mask from nifti image 
 
 '''
-
+import glob
 from StateSpace import CorrelateTasksWithGradients
 
+#CorrelateTasksWithGradients.corrTasks("/Users/willstrawson/Documents/PhD/repos/StateSpace/scratch")
+
 CorrelateTasksWithGradients.corrTasks("/Users/willstrawson/Documents/PhD/repos/StateSpace/scratch",
-                                      "spearman",
-                                      saveMaskedimgs = True)
+inputfiles=glob.glob('/Users/willstrawson/Documents/PhD/Rotation_2/fMRI/lev_2/allps_avhsruns_0.2.gfeat/cope10.feat/stats/zstat*.nii.gz'))
+
+
 
 
         
