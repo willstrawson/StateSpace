c6ee4af1dd6cd4d8d3615f0e3f051a24bf48bc5f
willstrawson <ws231@sussex.ac.uk>
2023-03-09 12:24:21 -0500

    used pkg_resources to access paths in mask making script

--- StateSpace/CreateBinarizedMask.py
+++ StateSpace/CreateBinarizedMask.py
@@ -7,15 +7,21 @@ import numpy as np
 import nibabel as nib
 import glob
 import os
+import pkg_resources
 
 def binMask():
-    repo_path = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
-    map_paths = sorted(glob.glob(os.path.join(repo_path, 'data/gradients/gradient*.nii.gz')))
+
+    gradient_subdir = pkg_resources.resource_filename('StateSpace','data/gradients')
+    gradient_paths = sorted(glob.glob(f'{gradient_subdir}/*nii.gz'))
+
+
+    #repo_path = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
+    #map_paths = sorted(glob.glob(os.path.join(repo_path, 'data/gradients/gradient*.nii.gz')))
 
     mask_list = []
 
     # loop over grads (only necessary for sanity - all masks should be the same, regardless of which grad they came from)
-    for map in map_paths:
+    for map in gradient_paths:
 
         map_array = nib.load(map).get_fdata()
     
@@ -26,8 +32,9 @@ def binMask():
         mask_list.append(mask)
 
     if np.array_equal(mask_list[0],mask_list[1]):
+        maskdir=pkg_resources.resource_filename('StateSpace', 'data/masks')
         # NOTE: See how we used the affine arg? this resolved weird aligmennt issue
         nib.save(nib.Nifti1Image(mask_list[0], affine=nib.load(map).affine), 
-        os.path.join(repo_path, 'data/masks/gradientmask_cortical.nii.gz'))
+        os.path.join(maskdir,'gradientmask_cortical.nii.gz'))
     else:
         print('Gradient 1 and 2 mask arrays do not match.')
